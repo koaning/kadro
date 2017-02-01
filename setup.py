054c77edae65a04f54b6bc5121f18065cc27df7d
Vincent Warmerdam <code@Vincents-MacBook-Pro.local>
2017-02-01 16:34:43 +0100

    v0.1 now assigned in setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='kadro',
+      version='0.1',
+      description='Pandas Wrapper with Composable Grammar Support',
+      author='Vincent D. Warmerdam',
+      author_email='vincentwarmerdam@gmail.com',
+      url='koaning.io',
+      packages=['kadro',],
+      install_requires=['pandas', 'numpy', 'pytest'],)
